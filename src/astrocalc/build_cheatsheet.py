5025c5ae6512948c40a97c5abbef63d73e8e0f92
Sam Gilbert <samthegliderpilot@gmail.com>
2026-01-09 19:43:49 -0500

    Adding elliptical velocity equation

--- src/astrocalc/build_cheatsheet.py
+++ src/astrocalc/build_cheatsheet.py
@@ -78,9 +78,10 @@ def generate_latex():
         kepler.circular_velocity(),
         kepler.escape_velocity(),
         kepler.semi_latus_rectum(),
+        kepler.velocity_elliptical(),
     ]
     circularEquations = [equation_methods[0], equation_methods[1], equation_methods[4]]
-    ellipticalEquations = [equation_methods[2], equation_methods[6], equation_methods[3]]
+    ellipticalEquations = [equation_methods[2], equation_methods[6], equation_methods[3], equation_methods[7]]
     parabolicEquations = [equation_methods[5]]
     hyperbolicEquations = []
     latex_lines = [
